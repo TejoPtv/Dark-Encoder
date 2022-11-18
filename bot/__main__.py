eb97956dee5ac42c5057b689edd76341c20fa9c2
ZuperSam <118423395+ZuperSam@users.noreply.github.com>
2022-11-18 15:21:22 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,12 +39,12 @@ from bot.plugins.status_message_fn import (
 
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
-sudo_users = "1666551439" 
-crf.append("30")
+sudo_users = "" 
+crf.append("28")
 codec.append("libx265")
-resolution.append("854x480")
+resolution.append("1280x720")
 preset.append("veryfast")
-audio_b.append("40k")
+audio_b.append("64k")
 # 🤣
 
 
